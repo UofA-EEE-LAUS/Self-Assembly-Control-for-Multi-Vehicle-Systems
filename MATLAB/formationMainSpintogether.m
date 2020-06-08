d8a3d598c9c67143ac52aa9f44e8f1f080b5ed64
Ziming Wang <58678104+Ziming-Wang9564@users.noreply.github.com>
2020-06-09 00:31:05 +0930

    Add files via upload

--- MATLAB/formationMainSpintogether.m
+++ MATLAB/formationMainSpintogether.m
@@ -40,14 +40,41 @@ if (clientID>-1)
         thetal02=atan(abs(y0-y2)/abs(x0-x1))*(180/pi);
         [xl0,yl0,xl1,yl1,xl2,yl2]=linedistanceformation(x0,y0,dl01,dl02,thetal01,thetal02);
         %Get 3 rovers' position
-        MoveThate2(xl1,yl1,pi/2,int2str(1),clientID,vrep);
-        MoveThate2(xl2,yl2,pi/2,int2str(2),clientID,vrep);
+        
+        MoveThate2(xl1,yl1,3*pi/2,int2str(1),clientID,vrep);
+        MoveThate2(xl2,yl2,3*pi/2,int2str(2),clientID,vrep);
         
         
         elapsedTime = toc;
     end
     pause(1);
+  for loop=1:5
+      tic;
+    [returnCode,rover0]=vrep.simxGetObjectHandle(clientID,strcat('rover0'),vrep.simx_opmode_blocking);
+    [returnCode,rover1]=vrep.simxGetObjectHandle(clientID,strcat('rover1'),vrep.simx_opmode_blocking);
+    [returnCode,rover2]=vrep.simxGetObjectHandle(clientID,strcat('rover2'),vrep.simx_opmode_blocking);
+    [returnCode,positions0]=vrep.simxGetObjectPosition(clientID,rover0,-1,vrep.simx_opmode_blocking);
+    [returnCode,positions1]=vrep.simxGetObjectPosition(clientID,rover1,-1,vrep.simx_opmode_blocking);
+    [returnCode,positions2]=vrep.simxGetObjectPosition(clientID,rover2,-1,vrep.simx_opmode_blocking);
+    position=[positions0; positions1; positions2];
+    positiont0=positions0;
+    positiont1=positions1;
+    positiont2=positions2;
     
+    xs0=positions0(:,1);
+    ys0=positions0(:,2);
+    xs1=positions1(:,1);
+    ys1=positions1(:,2);
+    xs2=positions2(:,1);
+    ys2=positions2(:,2);
+    dt01=sqrt((abs(xs0-xs1)^2)+(abs(ys0-ys1)^2));
+    dt02=sqrt((abs(xs0-xs2)^2)+(abs(ys0-ys2)^2));
+    
+    gripper(dt01,int2str(1),clientID,vrep);
+    gripper(dt02,int2str(2),clientID,vrep);
+    
+  end
+    pause(3);
     
     
     for loop = 1:2
@@ -74,12 +101,12 @@ if (clientID>-1)
         %Get 3 rovers' position
         MoveThate2(xl1,yl1,pi/2,int2str(1),clientID,vrep);
         MoveThate2(xs2,ys2,pi/2,int2str(2),clientID,vrep);
-       
+        
         
         elapsedTime = toc;
     end
     pause(1);
-      for loop = 1:3
+    for loop = 1:3
         tic
         %Get 3 rovers' position
         [returnCode,rover0]=vrep.simxGetObjectHandle(clientID,strcat('rover0'),vrep.simx_opmode_blocking);
@@ -104,11 +131,11 @@ if (clientID>-1)
         thetat01=atan(abs(ys0-ys1)/abs(xs0-xs1))*(180/pi);
         thetat02=atan(abs(ys0-ys2)/abs(xs0-xs1))*(180/pi);
         [xt0,yt0,xt1,yt1,xt2,yt2]=tridistanceformation(xs0,ys0,dt01,dt02,thetat01,thetat02);
-          %Get 3 rovers' position
+        %Get 3 rovers' position
         MoveThate2(xt0,yt0,-pi/2,int2str(0),clientID,vrep);
         MoveThate2(xt1,yt1,pi/3,int2str(1),clientID,vrep);
         MoveThate2(xt2,yt2,2*pi/3,int2str(2),clientID,vrep);
-       
+        
         
         elapsedTime = toc;
     end
