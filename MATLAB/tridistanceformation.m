e1f5ded2c576d2d37111bbb0386569f6b18c3323
Yangkaiwei Zhou <61771614+j448151734@users.noreply.github.com>
2020-06-10 10:52:45 +0930

    Update tridistanceformation.m

--- MATLAB/tridistanceformation.m
+++ MATLAB/tridistanceformation.m
@@ -1,6 +1,10 @@
-function [xr0,yr0,xr1,yr1,xr2,yr2]=tridistanceformation(x0,y0,d01,d02,theta01,theta02)
+function [xr0,yr0,xr1,yr1,xr2,yr2]=tridistanceformation(x0,y0,x1,y1,x2,y2)
 for xr0=x0
     for yr0=y0
+        d01=sqrt((abs(x0-x1)^2)+(abs(y0-y1)^2));
+        d02=sqrt((abs(x0-x2)^2)+(abs(y0-y2)^2));
+        theta01=atan(abs(y0-y1)/abs(x0-x1))*(180/pi);
+        theta02=atan(abs(y0-y2)/abs(x0-x2))*(180/pi);
         if theta01~=150
             theta01=150;
             if  d01~=0.35
